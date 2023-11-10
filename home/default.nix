d3186e161954fdf40cfaa9cd6e1c7c88d3a79dc2
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-11-10 03:33:09 +0300

    home manager

---
+++ home/default.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, username, ... }:
+
+{
+  imports = [
+    ./desktop
+    # ./hyprland/hyprland.nix
+  ];
+
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
+
+  # set cursor size and dpi for 4k monitor
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  home.stateVersion = "23.05";
+  programs.home-manager.enable = true;
+}
---
+++ home/desktop/neovim.nix
@@ -0,0 +1,68 @@
+	{
+  pkgs,
+  astronvim,
+  ...
+}:
+###############################################################################
+#
+#  AstroNvim's configuration and all its dependencies(lsp, formatter, etc.)
+#
+#e#############################################################################
+{
+  #xdg.configFile = {
+    # astronvim's config
+    #"nvim".source = astronvim;
+
+    # my custom astronvim config, astronvim will load it after base config
+    # https://github.com/AstroNvim/AstroNvim/blob/v3.32.0/lua/astronvim/bootstrap.lua#L15-L16
+    #"astronvim/lua/user".source = ./astronvim_user;
+  #};
+
+  nixpkgs.config = {
+    programs.npm.npmrc = ''
+      prefix = ''${HOME}/.npm-global
+    '';
+  };
+
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+
+      viAlias = true;
+      vimAlias = true;
+
+      withPython3 = true;
+      withNodeJs = true;
+      extraPackages = with pkgs; [];
+
+      #configFile.source = ../../dotfiles/.config/nvim;
+
+      # currently we use lazy.nvim as neovim's package manager, so comment this one.
+      # plugins = with pkgs.vimPlugins; [
+      #   # search all the plugins using https://search.nixos.org/packages
+      # ];
+    };
+  };
+  home = {
+
+    file.".config/nvim" = {
+      source = ../../dotfiles/.config/nvim;
+      recursive = true;
+    };
+
+    packages = with pkgs;
+      [
+        #-- c/c++
+        cmake
+        gnumake
+        checkmake
+        gcc # c/c++ compiler, required by nvim-treesitter!
+
+        #-- Optional Requirements:
+        gdu # disk usage analyzer, required by AstroNvim
+        ripgrep # fast search tool, required by AstroNvim's '<leader>fw'(<leader> is space key)
+	verible
+      ];
+  };
+}
