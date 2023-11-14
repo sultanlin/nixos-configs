a807f99800af46ca5fabfdbcd1ea8fa8b7688081
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-11-15 02:53:02 +0300

    remove mason-tool

--- dotfiles/.config/nvim/lua/sultan/plugins/lsp/lsp.lua
+++ dotfiles/.config/nvim/lua/sultan/plugins/lsp/lsp.lua
@@ -119,16 +119,5 @@ return {
                 }
             end,
         })
-
-        mason_tool_installer.setup({
-            ensure_installed = {
-                "prettier", -- prettier formatter
-                "stylua", -- lua formatter
-                "isort", -- python formatter
-                "black", -- python formatter
-                "pylint", -- python linter
-                "eslint_d", -- js linter
-            },
-        })
     end,
 }
