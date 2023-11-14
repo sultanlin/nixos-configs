6d465e6d38d59128b52206be9e6faf7e26330c0d
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-11-15 02:31:51 +0300

    added icons etc, experimental

--- dotfiles/.config/nvim/lua/sultan/lazy.lua
+++ dotfiles/.config/nvim/lua/sultan/lazy.lua
@@ -18,6 +18,39 @@ require("lazy").setup({
 --     install = {
 --       colorscheme = "carbonfox",
 --     }
+  ui = {
+      -- A number <1 is a percentage, >1 is a fixed size.
+      size = { width = 0.8, height = 0.8 },
+      wrap = true,  -- `true`: Wrap the lines in the UI.  ( Looks a bit wonky without this. )
+
+      -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+      -- TODO: Set the custom border here.
+      border = "rounded",
+
+      icons = {
+          cmd         = " ",     -- TODO: Fix: ''     ( Can't see this )
+          config      = " ",
+          event       = " ",     -- TODO: Fix: ''     ( Can't see this )
+          ft          = " ",
+          init        = " ",
+          import      = " ",
+          keys        = " ",
+          lazy        = "󰒲 ",
+          loaded      = " ",
+          not_loaded  = " ",
+          plugin      = " ",
+          runtime     = " ",
+          source      = " ",
+          start       = "🟆 ",     -- TODO: Fix: ''     ( Can't see this )
+          task        = "✔ ",
+          list = {
+              "∈",
+              "➜",
+              "‒",
+              "·",
+          },
+      },
+  },
 })
 -- require("lazy").setup("pluginlist")
 
