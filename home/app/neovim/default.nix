e38464c784ea64ab7cac55f3007bba28af0465c9
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-11-24 02:14:34 +0300

    add gruvbox colorscheme on startup

--- home/app/neovim/default.nix
+++ home/app/neovim/default.nix
@@ -29,10 +29,10 @@
 
   programs = {
     neovim = {
-      let
-        toLua = str: "lua << EOF\n${str}\nEOF\n";
-        toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
-      in
+      #let
+      #  toLua = str: "lua << EOF\n${str}\nEOF\n";
+      #  toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+      #in
       enable = true;
       defaultEditor = true;
 
