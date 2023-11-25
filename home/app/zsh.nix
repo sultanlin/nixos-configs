8f8a4dafe9721a26a8ad1492d06731e34b3d2c48
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-11-25 04:54:58 +0300

    adding features to zsh

--- home/app/zsh.nix
+++ home/app/zsh.nix
@@ -41,23 +41,23 @@
       '';
     };
 
-    user.packages = with pkgs; [
-      zsh
-      nix-zsh-completions
-      bat
-      exa
-      fasd
-      fd
-      fzf
-      jq
-      ripgrep
-      tldr
-    ];
+    # user.packages = with pkgs; [
+    #   zsh
+    #   nix-zsh-completions
+    #   bat
+    #   exa
+    #   fasd
+    #   fd
+    #   fzf
+    #   jq
+    #   ripgrep
+    #   tldr
+    # ];
     #
-    # env = {
-    #   ZDOTDIR   = "$XDG_CONFIG_HOME/zsh";
-    #   ZSH_CACHE = "$XDG_CACHE_HOME/zsh";
-    #   ZGEN_DIR  = "$XDG_DATA_HOME/zgenom";
-    # };
+    env = {
+      ZDOTDIR   = "$XDG_CONFIG_HOME/zsh";
+      ZSH_CACHE = "$XDG_CACHE_HOME/zsh";
+      ZGEN_DIR  = "$XDG_DATA_HOME/zgenom";
+    };
   };
 }
