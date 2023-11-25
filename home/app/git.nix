62f689d9549ae2320b0a7b12413bbd1940bf739c
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-11-25 05:00:22 +0300

    adding features to zsh

--- home/app/git.nix
+++ home/app/git.nix
@@ -18,7 +18,7 @@
   home.packages = with pkgs; [
     # Automatically trims your branches whose tracking remote refs are merged or gone
     # It's really useful when you work on a project for a long time.
-    # git-trim
+    git-trim
   ];
 
   programs.git = {
