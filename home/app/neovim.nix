41451c3cc29ecc598c885e55a4b0ebddccd71970
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-11-25 17:57:01 +0300

    neovim improve config out of symlink

--- home/app/neovim.nix
+++ home/app/neovim.nix
@@ -49,6 +49,8 @@
     file."nvim" = {
       # source = config.lib.file.mkOutOfStoreSymlink ../../dotfiles/.config/nvim;
       source = config.lib.file.mkOutOfStoreSymlink "/home/sultan/nixos-config/dotfiles/.config/nvim";
+      source = config.lib.file.mkOutOfStoreSymlink builtins.toString ../../dotfiles/.config/nvim;
+      # current_dir = builtins.toString ../../dotfiles/.config/nvim;
       # ./nvim;
       # source = ../../dotfiles/.config/nvim;
       # source = /home/sultan/dotfiles/.config/nvim;
