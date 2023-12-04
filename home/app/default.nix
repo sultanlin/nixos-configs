04ea1e9b06a5c014f53bd1e0b6c658c92f1e800a
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-12-05 01:03:22 +0300

    nixos neovim config changes

--- home/app/default.nix
+++ home/app/default.nix
@@ -1,46 +1,4 @@
-{pkgs, config, specialArgs, ...}: 
-let
-  dotfilesConfig = specialArgs.dotfilesConfig;
-in 
-{
-  imports = [
-    #./creative.nix
-    #./gtk.nix
-    #./immutable-file.nix
-    #./ssh.nix
-    #./wallpaper.nix
-    #./xdg.nix
-    ./bash.nix
-    ./bat.nix
-    ./btop.nix
-    ./git.nix
-    ./media.nix
-    # ./neovim.nix
-    ./neovim
-    #./shell
-    ./starship.nix
-    ./terminal
-    ./tmux.nix
-    ./zsh.nix
-    ./direnv.nix
-    ./shell/eza.nix
-  ];
 
-
-
-  home.packages = with pkgs; [
-    # GUI apps
-    insomnia # REST client
-    wireshark # network analyzer
-
-    # e-book viewer(.epub/.mobi/...)
-    # do not support .pdf
-    foliate
-
-    # instant messaging
-    telegram-desktop
-    discord
-    qq # https://github.com/NixOS/nixpkgs/tree/master/pkgs/applications/networking/instant-messengers/qq
 
     # remote desktop(rdp connect)
     remmina
@@ -63,7 +21,7 @@ in
       # source = config.lib.file.mkOutOfStoreSymlink "${dotfilesConfig}/nvim";
       source = config.lib.file.mkOutOfStoreSymlink "${dotfilesConfig}/nvim-nixos";
       recursive = true;
-      target = ".config/nvim";
+      # target = ".config/nvim";
     };
   };
 }
