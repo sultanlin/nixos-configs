2179d2a93506180a70efd58e1a3422e3344a59ae
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-12-16 00:55:40 +0300

    nixos hosts add wsl

--- flake.nix
+++ flake.nix
@@ -1,104 +1,130 @@
-
 {
   description = "Nix and NixOS System Flake Configuration";
 
-  outputs = inputs @ { self, nixpkgs-unstable, nixpkgs-stable, home-manager, ... }:   # Function telling flake which inputs to use
-    let
-        hostname = "vm"; #current system/profile
-        username = "sultan";
-        dotfilesConfig = "/home/sultan/nixos-config/dotfiles/config";
-	userfullname = "Sultan Linjawi";
-	useremail = "skorpion219@hotmail.co.uk";
-	
-	x64_system = "x86_64-linux";
-	allSystems = [ x64_system ];
+  outputs = inputs @ {
+    self,
+    nixpkgs-unstable,
+    nixpkgs-stable,
+    home-manager,
+    nixos-wsl,
+    ...
+  }:
+  # Function telling flake which inputs to use
+  let
+    hostname = "vm"; #current system/profile
+    username = "sultan";
+    dotfilesConfig = "/home/sultan/nixos-config/dotfiles/config";
+    userfullname = "Sultan Linjawi";
+    useremail = "skorpion219@hotmail.co.uk";
 
-	nixosSystem = import ./hosts;
+    x64_system = "x86_64-linux";
+    allSystems = [x64_system];
 
-	vm_modules = {
-	  nixos-modules = [
-	    ./hosts/vm
-	    ./modules/hyprland.nix
-	  ];
-	  home-module = import ./home;
-	};
-	
-	desktop_modules = {
-	  nixos-modules = [
-	    ./hosts/desktop
-	    ./modules/hyprland.nix
-	  ];
-	  home-module = import ./home;
-	};
+    nixosSystem = import ./hosts;
 
-	laptop_modules = {
-	  nixos-modules = [
-	    ./hosts/laptop
-	    ./modules/hyprland.nix
-	  ];
-	  home-module = import ./home;
-	};
-
-	x64_specialArgs =
-          {
-            inherit username userfullname useremail hostname dotfilesConfig;
-            # use unstable branch for some packages to get the latest updates
-            pkgs-stable = import nixpkgs-stable {
-              system = x64_system; # refer the `system` parameter form outer scope recursively
-              # To use chrome, we need to allow the installation of non-free software
-              config.allowUnfree = true;
-            };
-          }
-	  // inputs;
-    in {
-      nixosConfigurations = let
-      	base_args = {
-	  inherit home-manager;
-	  nixpkgs = nixpkgs-unstable;
-	  system = x64_system;
-	  specialArgs = x64_specialArgs;
-	};
-      in {
-        # vm with hyprland compositor
-	vm = nixosSystem (vm_modules // base_args);
-
-        # desktop with hyprland compositor
-	#desktop = nixosSystem (desktop_modules // base_args);
-
-        # laptop with hyprland compositor
-	#laptop = nixosSystem (laptop_modules // base_args);
-      };
-
+    vm_modules = {
+      nixos-modules = [
+        ./hosts/vm
+        ./modules/hyprland.nix
+      ];
+      home-module = import ./home;
     };
 
-  inputs =                                                              # References Used by Flake
-    {
-      nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";         	# Unstable Nix Packages (Default)
-      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";          # Stable Nix Packages 
+    wsl_modules = {
+      nixos-modules = [
+        nixos-wsl.nixosModules.wsl
+        ./hosts/wsl
+        ./modules/hyprland.nix
+      ];
+      home-module = import ./home;
+    };
 
-    # home-manager, used for managing user configuration
-      home-manager = {                                                      # User Environment Manager
+    desktop_modules = {
+      nixos-modules = [
+        ./hosts/desktop
+        ./modules/hyprland.nix
+      ];
+      home-module = import ./home;
+    };
+
+    laptop_modules = {
+      nixos-modules = [
+        ./hosts/laptop
+        ./modules/hyprland.nix
+      ];
+      home-module = import ./home;
+    };
+
+    x64_specialArgs =
+      {
+        inherit username userfullname useremail hostname dotfilesConfig;
+        # use unstable branch for some packages to get the latest updates
+        pkgs-stable = import nixpkgs-stable {
+          system = x64_system; # refer the `system` parameter form outer scope recursively
+          # To use chrome, we need to allow the installation of non-free software
+          config.allowUnfree = true;
+        };
+      }
+      // inputs;
+  in {
+    nixosConfigurations = let
+      base_args = {
+        inherit home-manager;
+        nixpkgs = nixpkgs-unstable;
+        system = x64_system;
+        specialArgs = x64_specialArgs;
+      };
+    in {
+      # vm with hyprland compositor
+      vm = nixosSystem (vm_modules // base_args);
+
+      # wsl with hyprland compositor
+      wsl = nixosSystem (wsl_modules // base_args);
+
+      # desktop with hyprland compositor
+      #desktop = nixosSystem (desktop_modules // base_args);
+
+      # laptop with hyprland compositor
+      #laptop = nixosSystem (laptop_modules // base_args);
+    };
+  };
+
+  inputs =
+    # References Used by Flake
+    {
+      nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable"; # Unstable Nix Packages (Default)
+      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05"; # Stable Nix Packages
+
+      # home-manager, used for managing user configuration
+      home-manager = {
+        # User Environment Manager
         url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs-unstable";
       };
 
       # modern window compositor
       hyprland.url = "github:hyprwm/Hyprland";
-    
+
       # community wayland nixpkgs
       nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
-    
+
+      # enables nixos in wsl
+      nixos-wsl = {
+        url = "github:nix-community/NixOS-WSL";
+        inputs.nixpkgs.follows = "nixpkgs-unstable";
+      };
+
       # anyrun - a wayland launcher
       anyrun = {
         url = "github:Kirottu/anyrun";
         inputs.nixpkgs.follows = "nixpkgs-unstable";
-        };
-  
+      };
+
       # secrets management, lock with git commit at 2023/7/15
       agenix.url = "github:ryantm/agenix/0d8c5325fc81daf00532e3e26c6752f7bcde1143";
-  
+
       ########################  Color Schemes  #########################################
-  
+
       # color scheme - catppuccin
       catppuccin-btop = {
         url = "github:catppuccin/btop";
@@ -141,20 +167,19 @@
         flake = false;
       };
     };
-  
-    # the nixConfig here only affects the flake itself, not the system configuration!
-    nixConfig = {
-      experimental-features = ["nix-command" "flakes"];
-  
-      # nix community's cache server
-      extra-substituters = [
-        "https://nix-community.cachix.org"
-        "https://nixpkgs-wayland.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-      ];
-    };
-  }
 
+  # the nixConfig here only affects the flake itself, not the system configuration!
+  nixConfig = {
+    experimental-features = ["nix-command" "flakes"];
+
+    # nix community's cache server
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+    ];
+  };
+}
---
+++ hosts/wsl/default.nix
@@ -0,0 +1,60 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+{
+  config,
+  lib,
+  pkgs,
+  username,
+  ...
+}: {
+  imports = [
+    ../../modules/nixos
+  ];
+
+  # Remove grub, conflicts with native systemd in wsl
+  boot.loader = lib.mkForce {};
+
+  wsl = {
+    enable = true;
+    defaultUser = "${username}";
+    startMenuLaunchers = true;
+    nativeSystemd = true;
+
+    # Enable native Docker support
+    # docker-native.enable = true;
+
+    # Enable integration with Docker Desktop (needs to be installed)
+    # docker-desktop.enable = true;
+  };
+
+  environment.systemPackages = [pkgs.git];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+  # FIXME: uncomment the next block to make vscode running in Windows "just work" with NixOS on WSL
+  # solution adapted from: https://github.com/K900/vscode-remote-workaround
+  # more information: https://github.com/nix-community/NixOS-WSL/issues/238 and https://github.com/nix-community/NixOS-WSL/issues/294
+  # systemd.user = {
+  #   paths.vscode-remote-workaround = {
+  #     wantedBy = ["default.target"];
+  #     pathConfig.PathChanged = "%h/.vscode-server/bin";
+  #   };
+  #   services.vscode-remote-workaround.script = ''
+  #     for i in ~/.vscode-server/bin/*; do
+  #       echo "Fixing vscode-server in $i..."
+  #       ln -sf ${pkgs.nodejs_18}/bin/node $i/node
+  #     done
+  #   '';
+  # };
+}
