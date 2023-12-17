dca7d11371a603574b15befc7bb157b9468b1862
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-12-17 18:30:48 +0300

    adding home packages

--- home/home.nix
+++ home/home.nix
@@ -10,13 +10,13 @@
   # Packages that should be installed to the user profile.
   home = {
     sessionVariables = {
-      # XDG_CACHE_HOME = "$HOME/.cache";
-      # XDG_CONFIG_HOME = "$HOME/.config";
-      # XDG_DATA_HOME = "$HOME/.local/share";
-      # XDG_STATE_HOME = "$HOME/.local/state";
-      # XDG_BIN_HOME = "$HOME/.local/bin"; # Not technically in the official xdg specification
-      # XDG_DESKTOP_DIR = "$HOME/desktop";
-      # XDG_DOWNLOAD_DIR = "$HOME/downloads";
+      XDG_CACHE_HOME = "$HOME/.cache";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_STATE_HOME = "$HOME/.local/state";
+      XDG_BIN_HOME = "$HOME/.local/bin"; # Not technically in the official xdg specification
+      XDG_DESKTOP_DIR = "$HOME/desktop";
+      XDG_DOWNLOAD_DIR = "$HOME/downloads";
       # AJLOW_OCAML_TOOLS = "dune merlin ocaml-lsp-server odoc ocamlformat utop
       # dune-release core core_unix base";
     };
