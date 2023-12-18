17d392fef01a103ff0afe7c7e343aa4eb92e29b5
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-12-18 04:34:13 +0300

    adding apache

--- modules/nixos/nginx.nix
+++ modules/nixos/nginx.nix
@@ -7,33 +7,34 @@
   hostname,
   ...
 }: {
-  # services.nginx = {
-  #   enable = true;
-  #   recommendedTlsSettings = true;
-  #   recommendedOptimisation = true;
-  #   recommendedGzipSettings = true;
-  #   recommendedProxySettings = true;
-  #   virtualHosts."sultanlinj.me" = {
-  #     # enableACME = true;
-  #     # forceSSL = true;
-  #     # root = "/home/sultan/Documents/dev-learning/docs";
-  #     locations."/home/sultan/Documents/dev-learning/docs".proxyPass = "http://localhost:8000";
-  #   };
-  #   # appendHttpConfig = "listen 127.0.0.1:80;";
-  # };
+  services.nginx = {
+    enable = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+    recommendedProxySettings = true;
+    virtualHosts."sultanlinj.me" = {
+      default = true;
+      # enableACME = true;
+      # forceSSL = true;
+      root = "/home/sultan/Documents/dev-learning/docs";
+      # locations."/home/sultan/Documents/dev-learning/docs".proxyPass = "http://localhost:8000";
+    };
+    # appendHttpConfig = "listen 127.0.0.1:80;";
+  };
   # security.acme = {
   #   acceptTerms = true;
   #   defaults.email = "foo@bar.com";
   # };
 
-  services.httpd.enable = true;
-  services.httpd.adminAddr = "webmaster@example.org";
-  # services.httpd.enablePHP = true; # oof... not a great idea in my opinion
-
-  services.httpd.virtualHosts."example.org" = {
-    documentRoot = "/var/www/example.org";
-    # want ssl + a let's encrypt certificate? add `forceSSL = true;` right here
-  };
+  # services.httpd.enable = true;
+  # services.httpd.adminAddr = "webmaster@example.org";
+  # # services.httpd.enablePHP = true; # oof... not a great idea in my opinion
+  #
+  # services.httpd.virtualHosts."example.org" = {
+  #   documentRoot = "/var/www/example.org";
+  #   # want ssl + a let's encrypt certificate? add `forceSSL = true;` right here
+  # };
 
   networking.firewall.allowedTCPPorts = [80 443];
 }
