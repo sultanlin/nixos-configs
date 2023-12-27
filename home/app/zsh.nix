7fd36a13a1d4654b5f60c16a6f2888924d478782
Sultan Linjawi <skorpion219@hotmail.co.uk>
2023-12-27 03:04:07 +0300

    zsh adding more .zshrc

--- home/app/zsh.nix
+++ home/app/zsh.nix
@@ -81,14 +81,28 @@
       '';
 
       completionInit = ''
-        # Basic auto/tab complete:
-        autoload -U compinit
-        zstyle ':completion:*' menu select
-        ignored-patterns '*.lock'
-        single-ignored show # didn't work
-        zmodload zsh/complist
-        compinit
-        _comp_options+=(globdots)		# Include hidden files.
+                # colours
+                autoload -U colors && colors	      # colours
+                autoload -U compinit && compinit    # basic completion
+                autoload -U compinit colors zcalc   # theming
+
+                # tab completion
+        zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' # Case insensitive tab completion
+        zstyle ':completion:*' list-colors "''${(s.:.)--color=auto}"                        # Colored completion (different colors for dirs/files/etc)
+        zstyle ':completion:*' rehash true                                                # automatically find new executables in path
+        zstyle ':completion:*' menu select                                                # Highlight menu selection
+
+        # completion
+        zstyle :compinstall ~/.config/zsh/.zshrc
+
+                #
+                # Basic auto/tab complete:
+                autoload -U compinit
+                ignored-patterns 'flake.lock'
+                single-ignored show # didn't work
+                zmodload zsh/complist
+                compinit
+                _comp_options+=(globdots)		# Include hidden files.
       '';
     };
   };
