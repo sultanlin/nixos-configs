58b57923045f9ee18657a9a454a8a78f9ba20095
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-05 00:19:00 +0300

    adding overlays in neovim

--- overlays/neovim.nix
+++ overlays/neovim.nix
@@ -1,35 +1,30 @@
-{sentiment-nvim, ...}: {
-  nixpkgs = {
-    overlays = [
-      (final: prev: {
-        vimPlugins =
-          prev.vimPlugins
-          // {
-            own-sentiment-nvim = prev.vimUtils.buildVimPlugin {
-              name = "sentiment.nvim";
-              src = sentiment-nvim;
-            };
-          };
-      })
-    ];
-  };
-  # nixpkgs = {
-  #   overlays = [
-  #     (
-  #       final: prev: let
-  #         simplePlugin = input: name:
-  #           prev.vimUtils.buildVimPlugin {
-  #             inherit name;
-  #             src = input;
-  #           };
-  #       in {
-  #         vimPlugins =
-  #           prev.vimPlugins
-  #           // {
-  #             sentiment-nvim = simplePlugin inputs.sentiment-nvim "sentiment.nvim";
-  #           };
-  #       }
-  #     )
-  #   ];
-  # };
-}
+{sentiment-nvim, ...}: (final: prev: {
+  vimPlugins =
+    prev.vimPlugins
+    // {
+      own-sentiment-nvim = prev.vimUtils.buildVimPlugin {
+        name = "sentiment.nvim";
+        src = sentiment-nvim;
+      };
+    };
+})
+# nixpkgs = {
+#   overlays = [
+#     (
+#       final: prev: let
+#         simplePlugin = input: name:
+#           prev.vimUtils.buildVimPlugin {
+#             inherit name;
+#             src = input;
+#           };
+#       in {
+#         vimPlugins =
+#           prev.vimPlugins
+#           // {
+#             sentiment-nvim = simplePlugin inputs.sentiment-nvim "sentiment.nvim";
+#           };
+#       }
+#     )
+#   ];
+# };
+
