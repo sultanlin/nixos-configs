07529d11626b749096df08f74f94d385fb41d35c
Sultan Me <me@me.me>
2024-01-09 19:34:39 +0300

    testing

--- modules/nixos/settings-system.nix
+++ modules/nixos/settings-system.nix
@@ -22,14 +22,23 @@
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
+      LC_ADDRESS = "ar_SA.UTF-8";
+      LC_IDENTIFICATION = "ar_SA.UTF-8";
+      LC_MEASUREMENT = "ar_SA.UTF-8";
+      LC_MONETARY = "ar_SA.UTF-8";
+      LC_NAME = "ar_SA.UTF-8";
+      LC_NUMERIC = "ar_SA.UTF-8";
+      LC_PAPER = "ar_SA.UTF-8";
+      LC_TELEPHONE = "ar_SA.UTF-8";
+#      LC_TIME = "ar_SA.UTF-8";
+#      LC_ADDRESS = "en_US.UTF-8";
+#      LC_IDENTIFICATION = "en_US.UTF-8";
+#      LC_MEASUREMENT = "en_US.UTF-8";
+#      LC_MONETARY = "en_US.UTF-8";
+#      LC_NAME = "en_US.UTF-8";
+#      LC_NUMERIC = "en_US.UTF-8";
+#      LC_PAPER = "en_US.UTF-8";
+#      LC_TELEPHONE = "en_US.UTF-8";
       LC_TIME = "en_US.UTF-8";
     };
   };
