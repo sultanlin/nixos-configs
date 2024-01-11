7889d0b028cb7faa6ed846072e22d157a8bd35b0
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-12 00:44:33 +0300

    adding chrome command flags

--- home/app/browser/default.nix
+++ home/app/browser/default.nix
@@ -19,21 +19,23 @@
     microsoft-edge
   ];
 
-  # source code: https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix
-  google-chrome = {
-    enable = true;
+  programs = {
+    # source code: https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix
+    google-chrome = {
+      enable = true;
 
-    commandLineArgs = [
-      # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
-      # (only supported by chromium/chrome at this time, not electron)
-      "--gtk-version=4"
-      "--enable-features=UseOzonePlatform"
-      "--ozone-platform=wayland"
-      # make it use text-input-v1, which works for kwin 5.27 and weston
-      "--enable-wayland-ime"
+      commandLineArgs = [
+        # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
+        # (only supported by chromium/chrome at this time, not electron)
+        "--gtk-version=4"
+        "--enable-features=UseOzonePlatform"
+        "--ozone-platform=wayland"
+        # make it use text-input-v1, which works for kwin 5.27 and weston
+        "--enable-wayland-ime"
 
-      # enable hardware acceleration - vulkan api
-      # "--enable-features=Vulkan"
-    ];
+        # enable hardware acceleration - vulkan api
+        # "--enable-features=Vulkan"
+      ];
+    };
   };
 }
