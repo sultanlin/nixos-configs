81f555ddefce7152924f0e3007dc04fd9d65998f
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-17 22:15:48 +0300

    adding hyprland config

--- home/app/hyprland.nix
+++ home/app/hyprland.nix
@@ -12,17 +12,17 @@
     enable = true;
     settings = {
       # source = "${nur-ryan4yin.packages.${pkgs.system}.catppuccin-hyprland}/themes/mocha.conf";
-      env = [
-        # "NIXOS_OZONE_WL,1" # for any ozone-based browser & electron apps to run on wayland
-        # "MOZ_ENABLE_WAYLAND,1" # for firefox to run on wayland
-        # "MOZ_WEBRENDER,1"
-        # # misc
-        # "_JAVA_AWT_WM_NONREPARENTING,1"
-        # "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-        # "QT_QPA_PLATFORM,wayland"
-        "SDL_VIDEODRIVER,wayland"
-        "GDK_BACKEND,wayland"
-      ];
+      # env = [
+      #   # "NIXOS_OZONE_WL,1" # for any ozone-based browser & electron apps to run on wayland
+      #   # "MOZ_ENABLE_WAYLAND,1" # for firefox to run on wayland
+      #   # "MOZ_WEBRENDER,1"
+      #   # # misc
+      #   # "_JAVA_AWT_WM_NONREPARENTING,1"
+      #   # "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+      #   # "QT_QPA_PLATFORM,wayland"
+      #   "SDL_VIDEODRIVER,wayland"
+      #   "GDK_BACKEND,wayland"
+      # ];
     };
     # package = hyprland.packages.${pkgs.system}.hyprland;
     # extraConfig = builtins.readFile ../conf/hyprland.conf;
