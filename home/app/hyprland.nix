7392bf00ab8c45adf6028a610e1618b9e904f073
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-17 22:13:57 +0300

    adding hyprland config

--- home/app/hyprland.nix
+++ home/app/hyprland.nix
@@ -24,7 +24,7 @@
         "GDK_BACKEND,wayland"
       ];
     };
-    package = hyprland.packages.${pkgs.system}.hyprland;
+    # package = hyprland.packages.${pkgs.system}.hyprland;
     # extraConfig = builtins.readFile ../conf/hyprland.conf;
     # gammastep/wallpaper-switcher need this to be enabled.
     systemd.enable = true;
