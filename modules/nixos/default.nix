c9dc91dbac5405020880e2416268fe018db33913
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-17 22:06:43 +0300

    refactoring

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -8,7 +8,7 @@
     ./fhs-fonts.nix
     ./user-group.nix
     ./settings-system.nix
-    ./boot-system.nix
+    # ./boot-system.nix
     ./apps-system.nix
     #../cifs-mount.nix
     #../nfs-mount.nix
