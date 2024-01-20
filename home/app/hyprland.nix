09942bac1094d8c046a7c1d79462314783ebe82b
Sultan Linjawi <skorpion219@hotmail.co.uk>
2024-01-20 18:12:38 +0300

    hyprland home-manager changes

--- home/app/hyprland.nix
+++ home/app/hyprland.nix
@@ -12,19 +12,19 @@
     enable = true;
     settings = {
       # source = "${nur-ryan4yin.packages.${pkgs.system}.catppuccin-hyprland}/themes/mocha.conf";
-      # env = [
-      #   # "NIXOS_OZONE_WL,1" # for any ozone-based browser & electron apps to run on wayland
-      #   # "MOZ_ENABLE_WAYLAND,1" # for firefox to run on wayland
-      #   # "MOZ_WEBRENDER,1"
-      #   # # misc
-      #   # "_JAVA_AWT_WM_NONREPARENTING,1"
-      #   # "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-      #   # "QT_QPA_PLATFORM,wayland"
-      #   "SDL_VIDEODRIVER,wayland"
-      #   "GDK_BACKEND,wayland"
-      # ];
+      env = [
+        # "NIXOS_OZONE_WL,1" # for any ozone-based browser & electron apps to run on wayland
+        # "MOZ_ENABLE_WAYLAND,1" # for firefox to run on wayland
+        # "MOZ_WEBRENDER,1"
+        # # misc
+        # "_JAVA_AWT_WM_NONREPARENTING,1"
+        # "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        # "QT_QPA_PLATFORM,wayland"
+        "SDL_VIDEODRIVER,wayland"
+        "GDK_BACKEND,wayland"
+      ];
     };
-    # package = hyprland.packages.${pkgs.system}.hyprland;
+    package = hyprland.packages.${pkgs.system}.hyprland;
     # extraConfig = builtins.readFile ../conf/hyprland.conf;
     # gammastep/wallpaper-switcher need this to be enabled.
     systemd.enable = true;
